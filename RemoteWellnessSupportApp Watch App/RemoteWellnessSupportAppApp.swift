aa68a1c22cc13897d9356b1cbde8fd477adb3c23
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2024-01-24 21:18:30 +0900

    chore: 環境構築初期状態

---
+++ RemoteWellnessSupportApp Watch App/RemoteWellnessSupportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteWellnessSupportAppApp.swift
+//  RemoteWellnessSupportApp Watch App
+//
+//  Created by 岩本雄貴 on 2024/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoteWellnessSupportApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RemoteWellnessSupportApp/RemoteWellnessSupportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteWellnessSupportAppApp.swift
+//  RemoteWellnessSupportApp
+//
+//  Created by 岩本雄貴 on 2024/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoteWellnessSupportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
