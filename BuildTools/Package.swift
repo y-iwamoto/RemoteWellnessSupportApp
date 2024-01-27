867d7036a8a663f40377a341b623f06f511a3180
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2024-01-27 21:36:46 +0900

    chore: swiftLintの追加

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -5,7 +5,8 @@ let package = Package(
     name: "BuildTools",
     platforms: [.macOS(.v10_11)],
     dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0")
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.49.0"),
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.53.0")
     ],
     targets: [.target(name: "BuildTools", path: "")]
 )
