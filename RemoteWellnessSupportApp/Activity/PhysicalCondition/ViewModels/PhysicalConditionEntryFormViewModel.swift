d14f2adef2c4dd5bc927a8624d912c3ab1a7d48f
y-iwamoto <klc.oka.iwa.tsu.gmiw@gmail.com>
2024-03-06 08:43:38 +0900

    feat: 頭痛記録の一覧と更新削除ができるように対応

--- RemoteWellnessSupportApp/Activity/PhysicalCondition/ViewModels/PhysicalConditionEntryFormViewModel.swift
+++ RemoteWellnessSupportApp/Activity/PhysicalCondition/ViewModels/PhysicalConditionEntryFormViewModel.swift
@@ -10,9 +10,15 @@ import Foundation
 @MainActor
 class PhysicalConditionEntryFormViewModel: ObservableObject {
     private let dataSource: PhysicalConditionDataSource
+    private let action: FormAction
+    private let physicalCondition: PhysicalCondition?
 
-    init(dataSource: PhysicalConditionDataSource = .shared) {
+    init(formAction: FormAction = .create, physicalCondition: PhysicalCondition? = nil, dataSource: PhysicalConditionDataSource = .shared) {
         self.dataSource = dataSource
+        action = formAction
+        self.physicalCondition = physicalCondition
+
+        setFormInitialValue()
     }
 
     @Published var selectedDateTime = Date()
@@ -20,6 +26,34 @@ class PhysicalConditionEntryFormViewModel: ObservableObject {
     @Published var selectedRating: PhysicalConditionRating?
     @Published var isErrorAlert = false
     @Published var errorMessage = ""
+
+    func formAction() -> Bool {
+        switch action {
+        case .create:
+            insertPhysicalCondition()
+        case .update:
+            updatePhysicalCondition()
+        }
+    }
+
+    func updatePhysicalCondition() -> Bool {
+        guard validateInputs() else {
+            return false
+        }
+        guard let updatedPhysicalCondition = updatePhysicalConditionValues() else {
+            setError(withMessage: "体調更新に失敗しました")
+            return false
+        }
+
+        do {
+            try dataSource.updatePhysicalCondition(physicalCondition: updatedPhysicalCondition)
+            return true
+        } catch {
+            print("update PhysicalCondition Error: \(error)")
+            setError(withMessage: "体調更新に失敗しました")
+            return false
+        }
+    }
 
     func insertPhysicalCondition() -> Bool {
         guard validateInputs() else {
@@ -38,6 +72,30 @@ class PhysicalConditionEntryFormViewModel: ObservableObject {
             setError(withMessage: "体調登録に失敗しました")
             return false
         }
+    }
+
+    private func setFormInitialValue() {
+        guard let item = physicalCondition else {
+            return
+        }
+
+        selectedDateTime = item.entryDate
+        memo = item.memo
+        selectedRating = PhysicalConditionRating(rawValue: item.rating)
+    }
+
+    private func updatePhysicalConditionValues() -> PhysicalCondition? {
+        guard let physicalCondition else {
+            return nil
+        }
+
+        guard let rating = selectedRating?.rawValue else { return nil }
+
+        physicalCondition.memo = memo
+        physicalCondition.rating = rating
+        physicalCondition.entryDate = selectedDateTime
+
+        return physicalCondition
     }
 
     private func validateInputs() -> Bool {
